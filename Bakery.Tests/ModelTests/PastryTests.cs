2063160f114abccfabfcc68c8eb1e75ecbd5d12d
Kate Skorija <kateskorija@gmail.com>
2020-07-17 09:47:16 -0700

    failing test for returning basic price of pastry

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -13,5 +13,13 @@ namespace Bakery.TestTools
       Pastry newPastry = new Pastry();
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
+    [TestMethod]
+    public void GetPrice_ReturnsPrice_Int()
+    {
+      int testPrice = 2;
+      Pastry newPastry = new Pastry();
+      int result = newPastry.Price;
+      Assert.AreEqual(testPrice, result);
+    }
   }
 }
