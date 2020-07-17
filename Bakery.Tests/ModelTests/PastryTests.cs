7771f0b4f4c6b38d693d0855dc6c62bdee4f8bd0
Kate Skorija <kateskorija@gmail.com>
2020-07-17 09:43:45 -0700

    failing test for creation Pastry class

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -1,3 +1,17 @@
-// using System;
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using Bakery.Models;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.TestTools
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
+}
